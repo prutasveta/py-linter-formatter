30d04c7137821a8562dc0d66e453c4b9ab4f221f
Svitlana <svetapruta@gmail.com>
2024-12-02 15:19:42 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,13 +1,25 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "failed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [
+        {"errors": value, "path": key, "status": "passed"}
+        if value == []
+        else format_single_linter_file(key, value)
+        for key, value in linter_report.items()
+    ]
